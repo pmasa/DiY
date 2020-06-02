c78cef85de6bd14a1f963ced5c491b2b8c62deed
pmasa <masamuna.pedro@hotmail.com>
2020-06-02 18:00:05 +0200

    first commit

---
+++ diy.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+from gpiozero import MotionSensor
+
+import threading 
+
+GPIO.setmode(GPIO.BCM)
+
+relay_pin = 27
+pir_sensor = 17
+
+GPIO.setup(relay_pin, GPIO.OUT)
+pir = MotionSensor(pir_sensor)
+
+print ("Waiting for 2 seconds")
+time.sleep(2)
+
+
+print ("Debut du processus...")
+
+
+try:
+    while True:
+        time.sleep(0.5)    
+        if pir.motion_detected:
+            
+            print("Demarrage pompe")
+            GPIO.output(relay_pin, GPIO.HIGH)
+            time.sleep(0.005)
+
+            print("Arret pompe")
+            GPIO.output(relay_pin, GPIO.LOW)
+            time.sleep(10)
+
+except KeyboardInterrupt:
+    print("CTRL-C: stoppe le programme.")
+finally:
+    print("Nettoyage des GPIO...")
+    GPIO.output(relay_pin, GPIO.LOW)
+    GPIO.cleanup()
+
+
